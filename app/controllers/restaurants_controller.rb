2bdcdc1dbb94997176a8af3ce350de6bbab0a7db
Saori Mita <saorimita4@gmail.com>
2022-09-01 13:13:28 +0200

    res

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,16 @@
 class RestaurantsController < ApplicationController
+  # before_action :set_restaurant
+
+  def index
+    @restaurants = Restaurant.all
+  end
 
   def show
+
   end
+
+  # private
+  # def set_restaurant
+  #   @restaurant = Restaurant(params[:id])
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
   get 'about', to: 'pages#about'
+  resources :categories do
+    resources :restaurants
+  end
+  resources :restaurants do
+    resources :reviews, only: %i[new create]
+  end
+  resources :restaurants, only: :destroy
+  resources :reviews, only: :destroy
 end
